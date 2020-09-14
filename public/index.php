1225c321b60e3654f64c1ebb55b90e507fdde5b8
Joao Pedro <jp.ssilva@live.com>
2020-09-14 11:22:03 -0300

    Gerando um comando sql

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Mateo Belini', 'email' => 'mateo@belini.com']);
-print_r($user);
-$user->email = "mateobelini@email.com";
-print_r($user->email);
+//echo $user->getSelect("id, name");
+
+echo User::getSelect(['id' => 1], 'id,name');
+echo "<br>";
+echo User::getSelect(['name' => "Chaves", 'email' => "chaves.cod3r.com.br"]);
