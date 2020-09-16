b45d69a444f0b6cda93ca08ec591ea1e692eb469
Joao Pedro <jp.ssilva@live.com>
2020-09-16 16:41:05 -0300

    Implementando o Login pt1

--- src/config/config.php
+++ src/config/config.php
@@ -5,5 +5,9 @@ setlocale(LC_ALL, 'pt-BR', 'pt-BR-utf8', 'portuguese');
 
 //Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
 
+
+//Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
+require_once(realpath(MODEL_PATH . '/Model.php'));
